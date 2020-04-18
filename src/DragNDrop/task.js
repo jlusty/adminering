ef00443e8bc251a4bcaef9d66b5c1c527c6428f0
John Lusty <54030459+jlusty@users.noreply.github.com>
2020-04-18 14:56:20 +0100

    Copy drag and drop example into project

---
+++ src/DragNDrop/task.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Draggable } from 'react-beautiful-dnd';
+
+const Container = styled.div`
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+  padding: 8px;
+  margin-bottom: 8px;
+  background-color: ${props => (props.isDragging ? 'lightgreen' : 'white')};
+`;
+
+const Task = ({ task, index }) => {
+  return (
+    <Draggable draggableId={task.id} index={index}>
+      {(provided, snapshot) => (
+        <Container
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+          isDragging={snapshot.isDragging}
+        >
+          {task.content}
+        </Container>
+      )}
+    </Draggable>
+  );
+};
+
+export default Task;
