73bcb3c2fc53308b20c2368aa2308fdbda04ff09
John Lusty <54030459+jlusty@users.noreply.github.com>
2020-04-19 13:24:59 +0100

    Allow reordering of columns

--- src/DragNDrop/DnD.js
+++ src/DragNDrop/DnD.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 // import 'normalize.css';
 import styled from 'styled-components';
-import { DragDropContext } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 import initialData from './initial-data';
 import Folder from './Folder';
 
@@ -14,7 +14,7 @@ const DnD = () => {
   const [data, setData] = useState(initialData);
 
   const onDragEnd = result => {
-    const { destination, source, draggableId } = result;
+    const { destination, source, draggableId, type } = result;
 
     if (!destination) {
       return;
@@ -24,6 +24,19 @@ const DnD = () => {
       destination.droppableId === source.droppableId &&
       destination.index === source.index
     ) {
+      return;
+    }
+
+    if (type === 'folder') {
+      const newFolderOrder = Array.from(data.folderOrder);
+      newFolderOrder.splice(source.index, 1);
+      newFolderOrder.splice(destination.index, 0, draggableId);
+
+      const newState = {
+        ...data,
+        folderOrder: newFolderOrder,
+      };
+      setData(newState);
       return;
     }
 
@@ -77,14 +90,26 @@ const DnD = () => {
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
-      <Container>
-        {data.folderOrder.map(folderId => {
-          const folder = data.folders[folderId];
-          const urls = folder.urlIds.map(urlId => data.urls[urlId]);
+      <Droppable droppableId="all-folders" direction="horizontal" type="folder">
+        {provided => (
+          <Container {...provided.droppableProps} ref={provided.innerRef}>
+            {data.folderOrder.map((folderId, index) => {
+              const folder = data.folders[folderId];
+              const urls = folder.urlIds.map(urlId => data.urls[urlId]);
 
-          return <Folder key={folder.id} folder={folder} urls={urls} />;
-        })}
-      </Container>
+              return (
+                <Folder
+                  key={folder.id}
+                  folder={folder}
+                  urls={urls}
+                  index={index}
+                />
+              );
+            })}
+            {provided.placeholder}
+          </Container>
+        )}
+      </Droppable>
     </DragDropContext>
   );
 };
--- src/DragNDrop/Folder.js
+++ src/DragNDrop/Folder.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Droppable } from 'react-beautiful-dnd';
+import { Draggable, Droppable } from 'react-beautiful-dnd';
 import DraggableUrl from './DraggableUrl';
 import DividerSection from './DividerSection';
 
 const Container = styled.div`
   margin: 8px;
   border: 1px solid lightgrey;
+  background-color: white;
   border-radius: 2px;
   width: 300px;
 
@@ -19,43 +20,51 @@ const Title = styled.h3`
 `;
 const UrlList = styled.div`
   padding: 8px;
-  background-color: ${props => (props.isDraggingOver ? 'skyblue' : 'white')};
+  background-color: ${props => (props.isDraggingOver ? 'skyblue' : 'inherit')};
   flex-grow: 1;
   min-height: 300px;
 `;
 
-const Folder = ({ folder, urls }) => {
+const Folder = ({ folder, urls, index }) => {
   return (
-    <Container>
-      <Title>{folder.title}</Title>
-      <Droppable droppableId={folder.id}>
-        {(provided, snapshot) => (
-          <UrlList
-            ref={provided.innerRef}
-            {...provided.droppableProps}
-            isDraggingOver={snapshot.isDraggingOver}
-          >
-            {urls.map((urlObj, index) => {
-              if (urlObj.type === 'url') {
-                return (
-                  <DraggableUrl key={urlObj.id} urlObj={urlObj} index={index} />
-                );
-              } else if (urlObj.type === 'divider') {
-                return (
-                  <DividerSection
-                    key={urlObj.id}
-                    urlObj={urlObj}
-                    index={index}
-                  />
-                );
-              }
-              return <></>;
-            })}
-            {provided.placeholder}
-          </UrlList>
-        )}
-      </Droppable>
-    </Container>
+    <Draggable draggableId={folder.id} index={index}>
+      {provided => (
+        <Container {...provided.draggableProps} ref={provided.innerRef}>
+          <Title {...provided.dragHandleProps}>{folder.title}</Title>
+          <Droppable droppableId={folder.id} type="url">
+            {(provided, snapshot) => (
+              <UrlList
+                ref={provided.innerRef}
+                {...provided.droppableProps}
+                isDraggingOver={snapshot.isDraggingOver}
+              >
+                {urls.map((urlObj, index) => {
+                  if (urlObj.type === 'url') {
+                    return (
+                      <DraggableUrl
+                        key={urlObj.id}
+                        urlObj={urlObj}
+                        index={index}
+                      />
+                    );
+                  } else if (urlObj.type === 'divider') {
+                    return (
+                      <DividerSection
+                        key={urlObj.id}
+                        urlObj={urlObj}
+                        index={index}
+                      />
+                    );
+                  }
+                  return <></>;
+                })}
+                {provided.placeholder}
+              </UrlList>
+            )}
+          </Droppable>
+        </Container>
+      )}
+    </Draggable>
   );
 };
 
