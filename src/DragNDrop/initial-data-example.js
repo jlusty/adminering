a7eea0a3dfa1fabd503e8f49b885a988ba270e99
John Lusty <54030459+jlusty@users.noreply.github.com>
2021-08-22 23:24:22 +0100

    Add initial data example

---
+++ src/DragNDrop/initial-data-example.js
@@ -0,0 +1,174 @@
+const initialData = {
+  urls: {
+    '84b64560-b67b-48f8-9200-f743237404fd': {
+      id: '84b64560-b67b-48f8-9200-f743237404fd',
+      type: 'url',
+      url: 'http://example.com/',
+      description: 'Example Domain',
+      groupNum: 0,
+    },
+    'dbd1f310-2a00-4ee0-8812-c5484ca46906': {
+      id: 'dbd1f310-2a00-4ee0-8812-c5484ca46906',
+      type: 'divider',
+      groupNum: 0,
+    },
+    '1ceb4419-5a25-4b0e-b9f1-6c8e225a424e': {
+      id: '1ceb4419-5a25-4b0e-b9f1-6c8e225a424e',
+      type: 'url',
+      url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+      description:
+        'Rick Astley - Never Gonna Give You Up (Official Music Video)',
+      siteName: 'YouTube',
+      groupNum: 1,
+    },
+    '3e88bbf6-fee3-4752-bb44-f8460084168c': {
+      id: '3e88bbf6-fee3-4752-bb44-f8460084168c',
+      type: 'url',
+      url: 'http://example.com/',
+      description: 'Example Domain',
+      groupNum: 1,
+    },
+    '6ea87d2c-3c16-4373-9ec2-fabc1c273398': {
+      id: '6ea87d2c-3c16-4373-9ec2-fabc1c273398',
+      type: 'url',
+      url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+      description:
+        'Rick Astley - Never Gonna Give You Up (Official Music Video)',
+      siteName: 'YouTube',
+      groupNum: 1,
+    },
+    'ff8c1dad-5e55-49c2-868b-358f052a0c10': {
+      id: 'ff8c1dad-5e55-49c2-868b-358f052a0c10',
+      type: 'url',
+      url: 'http://example.com/',
+      description: 'Example Domain',
+      groupNum: 1,
+    },
+    '698571e1-6472-43b4-838a-ffe685a5b4e3': {
+      id: '698571e1-6472-43b4-838a-ffe685a5b4e3',
+      type: 'url',
+      url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+      description:
+        'Rick Astley - Never Gonna Give You Up (Official Music Video)',
+      siteName: 'YouTube',
+      groupNum: 1,
+    },
+    'eba6a77a-fce4-4467-ab89-98c8dcd46084': {
+      id: 'eba6a77a-fce4-4467-ab89-98c8dcd46084',
+      type: 'url',
+      url: 'http://example.com/',
+      description: 'Example Domain',
+      groupNum: 1,
+    },
+    'd15e677e-92f7-4b41-8a2e-b31dd53b1aad': {
+      id: 'd15e677e-92f7-4b41-8a2e-b31dd53b1aad',
+      type: 'url',
+      url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+      description:
+        'Rick Astley - Never Gonna Give You Up (Official Music Video)',
+      siteName: 'YouTube',
+      groupNum: 1,
+    },
+    '48183c1d-2819-4bc2-91ee-392a42e7d8ed': {
+      id: '48183c1d-2819-4bc2-91ee-392a42e7d8ed',
+      type: 'url',
+      url: 'http://example.com/',
+      description: 'Example Domain',
+      groupNum: 1,
+    },
+    '87c14c7a-dc92-4881-b25e-2e1e0dc315be': {
+      id: '87c14c7a-dc92-4881-b25e-2e1e0dc315be',
+      type: 'divider',
+      groupNum: 1,
+    },
+    'a528ec82-9bd6-49e6-8af0-fd01a5c9d3a3': {
+      id: 'a528ec82-9bd6-49e6-8af0-fd01a5c9d3a3',
+      type: 'url',
+      url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+      description:
+        'Rick Astley - Never Gonna Give You Up (Official Music Video)',
+      siteName: 'YouTube',
+      groupNum: 2,
+    },
+    '6d9e9b7d-4674-4ed7-bb29-09df13f48207': {
+      id: '6d9e9b7d-4674-4ed7-bb29-09df13f48207',
+      type: 'divider',
+      groupNum: 2,
+    },
+    '3a535b6d-3528-44fa-bfea-fc14ad6e1737': {
+      id: '3a535b6d-3528-44fa-bfea-fc14ad6e1737',
+      type: 'url',
+      url: 'http://example.com/',
+      description: 'Example Domain',
+      groupNum: 3,
+    },
+    '7815bc97-ed8f-4b66-8841-f7d95d19f042': {
+      id: '7815bc97-ed8f-4b66-8841-f7d95d19f042',
+      type: 'url',
+      url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+      description:
+        'Rick Astley - Never Gonna Give You Up (Official Music Video)',
+      siteName: 'YouTube',
+      groupNum: 3,
+    },
+    '02b7980a-7281-4be8-833e-4aee9c259b76': {
+      id: '02b7980a-7281-4be8-833e-4aee9c259b76',
+      type: 'url',
+      url: 'http://example.com/',
+      description: 'Example Domain',
+      groupNum: 3,
+    },
+    '863f5299-4b77-42a1-8058-8dd2325f7cb2': {
+      id: '863f5299-4b77-42a1-8058-8dd2325f7cb2',
+      type: 'url',
+      url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+      description:
+        'Rick Astley - Never Gonna Give You Up (Official Music Video)',
+      siteName: 'YouTube',
+      groupNum: 3,
+    },
+  },
+  folders: {
+    'folder-1': {
+      id: 'folder-1',
+      title: 'Initial folder',
+      urlIds: [
+        '84b64560-b67b-48f8-9200-f743237404fd',
+        'dbd1f310-2a00-4ee0-8812-c5484ca46906',
+        '1ceb4419-5a25-4b0e-b9f1-6c8e225a424e',
+        '3e88bbf6-fee3-4752-bb44-f8460084168c',
+        '6ea87d2c-3c16-4373-9ec2-fabc1c273398',
+        'ff8c1dad-5e55-49c2-868b-358f052a0c10',
+        '698571e1-6472-43b4-838a-ffe685a5b4e3',
+        'eba6a77a-fce4-4467-ab89-98c8dcd46084',
+        'd15e677e-92f7-4b41-8a2e-b31dd53b1aad',
+        '48183c1d-2819-4bc2-91ee-392a42e7d8ed',
+        '87c14c7a-dc92-4881-b25e-2e1e0dc315be',
+        'a528ec82-9bd6-49e6-8af0-fd01a5c9d3a3',
+        '6d9e9b7d-4674-4ed7-bb29-09df13f48207',
+        '3a535b6d-3528-44fa-bfea-fc14ad6e1737',
+        '7815bc97-ed8f-4b66-8841-f7d95d19f042',
+        '02b7980a-7281-4be8-833e-4aee9c259b76',
+        '863f5299-4b77-42a1-8058-8dd2325f7cb2',
+      ],
+    },
+    'folder-2': { id: 'folder-2', title: 'Second folder', urlIds: [] },
+  },
+  folderColumns: {
+    'folder-column-1': {
+      id: 'folder-column-1',
+      title: 'Initial folder column',
+      direction: 'vertical',
+      folderIds: [],
+    },
+    'folder-column-2': {
+      id: 'folder-column-2',
+      title: 'Second folder column',
+      direction: 'horizontal',
+      folderIds: ['folder-1', 'folder-2'],
+    },
+  },
+  folderColumnsOrder: ['folder-column-1', 'folder-column-2'],
+};
+
+export default initialData;
